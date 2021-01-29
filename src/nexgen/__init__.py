f008a47d7526fc69fd117453def720fcad61aa1a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-01-29 15:19:38 +0000

    Bump version: 0.1.0 → 0.2.0

--- src/nexgen/__init__.py
+++ src/nexgen/__init__.py
@@ -4,7 +4,7 @@ Code goes here
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 def ohai():
