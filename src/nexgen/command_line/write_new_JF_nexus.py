b94019579bc75f0b5bd1b4d577521016ecc69d45
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2021-04-21 12:21:17 +0100

    Finished nxsample, added nxinstrument and part of nxdetector, tbc.

--- src/nexgen/command_line/write_new_JF_nexus.py
+++ src/nexgen/command_line/write_new_JF_nexus.py
@@ -11,7 +11,7 @@ from nexgen import create_attributes
 def find_scan_axis(gonio):
     # Dumb way
     for k in gonio.keys():
-        if "increment" in k and gonio[k] != 0:
+        if "increment" in k and gonio[k][()] != 0:
             return k.split("_")[0]
 
 
@@ -30,14 +30,17 @@ def main(infile: h5py.File, outfile: h5py.File):
     nxentry = outfile.create_group("entry")
     create_attributes(nxentry, ("NX_class",), ("NXentry",))
 
+    # Copy definition (Nxmx)
+    infile.copy("entry/definition", nxentry)
+
     # Start by figureing out which one is the scan axis
     ax = find_scan_axis(infile["entry/sample/goniometer"])
 
     # Data: /entry/data
     # Start by copying all data links, without attributes
-    fin.copy("entry/data", nxentry, without_attrs=True)
+    infile.copy("entry/data", nxentry, without_attrs=True)
     # Add scan axis w/ attributes
-    fin["entry/sample/goniometer"].copy(ax, nxentry["data"], without_attrs=True)
+    infile["entry/sample/goniometer"].copy(ax, nxentry["data"], without_attrs=True)
     create_attributes(
         nxentry["data"][ax],
         ("depends_on", "transformation_type", "units", "vector"),
@@ -54,11 +57,15 @@ def main(infile: h5py.File, outfile: h5py.File):
     nxsample = nxentry.create_group("sample")
     create_attributes(nxsample, ("NX_class",), ("NXsample",))
     # Copy beam as is, with attributes
-    fin["entry/sample"].copy("beam", nxsample)
+    infile["entry/sample"].copy("beam", nxsample)
     # Write scan_axis with correct attributes (this is just a link)
     nxax = nxsample.create_group("sample_" + ax)
     create_attributes(nxax, ("NX_class",), ("NXpositioner",))
     nxax[ax] = outfile["entry/data/" + ax]
+    # TODO also add increments, end etc ...
+    for k in infile["entry/sample"]:
+        if ax in k and k != ax:
+            infile["entry/sample"].copy(k, nxax, without_attrs=True)
     # Write NXtransformations (another link)
     nxtr = nxsample.create_group("transformations")
     create_attributes(nxtr, ("NX_class",), ("NXtransformations",))
@@ -69,11 +76,39 @@ def main(infile: h5py.File, outfile: h5py.File):
     )
 
     # Instrument: /entry/data/instrument
+    nxinstrument = nxentry.create_group("instrument")
+    create_attributes(nxinstrument, ("NX_class",), ("NXinstrument",))  # short_name ?
+    # Copy JF1M (NXdetector_group) in full
+    infile["entry/instrument"].copy("JF1M", nxinstrument)
+    # Add NXsource
+    nxsource = nxinstrument.create_group("source")
+    create_attributes(nxsource, ("NX_class",), ("NXsource",))
+    nxsource.create_dataset("name", data=np.string_("Diamond Light Source"))
+    create_attributes(nxsource["name"], ("short_name",), ("DLS",))
+    nxsource.create_dataset("type", data=np.string_("Synchrotron X-ray Source"))
+    # Copy NXattenuator from filter
+    infile["entry/instrument"].copy("filter", nxinstrument, name="attenuator")
+    # Nxbeam is a link to the one in sample
+    nxinstrument["beam"] = outfile["entry/sample/beam"]
 
     # Detector: /entry/data/instrument/detector
-    # Shallow copy ?
+    nxdetector = nxinstrument.create_group("detector")
+    create_attributes(nxdetector, ("NX_class",), ("NXdetector",))
+    # What about detector depends_on dataset ?
+    # Copy just the datasets
+    for k in infile["entry/instrument/detector"].keys():
+        if type(infile["entry/instrument/detector"]) is h5py.Dataset:
+            infile["entry/instrument/detector"].copy(k, nxdetector)
     # Module: /entry/data/instrument/detector/module
-    print("All done")
+    nxmodule = nxdetector.create_group("module")
+    create_attributes(nxmodule, ("NX_class",), ("NXdetector_module",))
+
+    # detectorSpecific
+    # Finally copy detector mask in correct place
+    nxdetector.create_dataset("pixel_mask_applied", data=False)  # might be true
+    infile["entry/instrument/detector/detectorSpecific"].copy("pixel_mask", nxdetector)
+
+    print("All done!")
 
 
 if __name__ == "__main__":
