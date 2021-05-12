6727f5a95a19fddb78379c2a7b848073cb917117
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2021-05-12 22:54:05 +0100

    Some work on the parser

--- src/nexgen/command_line/create_nexus.py
+++ src/nexgen/command_line/create_nexus.py
@@ -5,14 +5,24 @@ Command line tool to generate a NeXus file.
 # import sys
 
 # sys.path.append("/home/uhz96441/local/Python3_dials/modules/nexgen/src/nexgen/")
+
+import os
+
+# import h5py
+import logging
+import argparse
+
 import freephil
+
+# import nexgen.phil
+
+# import writer
+
+logger = logging.getLogger("NeXusWriter")
 
 master_phil = freephil.parse(
     """
     output {
-      data_file_template = nexus_%06d.h5
-        .type = path
-        .help = "Filename template for data files"
       master_file_name = nexus_master.h5
         .type = path
         .help = "Filename for master file"
@@ -31,21 +41,46 @@ master_phil = freephil.parse(
         .help = "Length of event stream per file"
     }
 
-    include scope phil.goniometer_scope
+    include scope nexgen.phil.goniometer_scope
 
-    include scope phil.beamline_scope
+    include scope nexgen.phil.beamline_scope
 
-    include scope phil.detector_scope
+    include scope nexgen.phil.detector_scope
     """,
     process_includes=True,
 )
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description="Parse parameters to generate a NeXus file."
+)
+parser.add_argument("--debug", action="store_const", const=True)
+parser.add_argument("phil", nargs="*")
+
 
 def main():
-    working_phil = master_phil.fetch()
-    working_phil.show()
-    # params = working_phil.extract()
+    # working_phil = master_phil.fetch()
+    # working_phil.show()
+    args = parser.parse_args()
+    cl = master_phil.command_line_argument_interpreter()
+    working_phil = master_phil.fetch(cl.process_and_fetch(args.phil))
+    params = working_phil.extract()
+
+    # Start logger
+    logdir = os.path.dirname(params.output.master_file_name)
+    logging.basicConfig(
+        filename=os.path.join(logdir, "NeXusWriter.log"),
+        format="%(message)s",
+        level="DEBUG",
+    )
+
+    # Check that the file extension is correct
+    ext = os.path.splitext(params.output.master_file_name)[1]
+    assert (ext == ".nxs") or (
+        ext == ".h5"
+    ), "Wrong file extension, please pass a .h5 or .nxs file."
 
 
 if __name__ == "__main__":
+    # args = parser.parse_args()
     main()
