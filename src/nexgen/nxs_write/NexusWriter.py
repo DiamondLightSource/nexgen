92b06a94b1aa93fc98ef1d94f55a951e7dd439a4
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2021-09-24 19:33:45 +0100

    Small tweak

--- src/nexgen/nxs_write/NexusWriter.py
+++ src/nexgen/nxs_write/NexusWriter.py
@@ -5,7 +5,7 @@ Writer for NeXus format files.
 import h5py
 
 from . import find_scan_axis, calculate_scan_range
-from data_tools import data_writer
+from ..nxswrite.data_tools import data_writer
 from NXclassWriters import (
     write_NXdata,
     write_NXinstrument,
