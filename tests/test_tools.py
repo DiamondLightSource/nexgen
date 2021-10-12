4d06c126de5eed7f4a556295e91d63806d09319c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2021-10-12 10:33:04 +0100

    Quick fix

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -18,8 +18,7 @@ def test_units_of_length():
     assert nexgen.units_of_length(100) == ureg.Quantity(100, "mm")
     # Check conversion to base units
     assert nexgen.units_of_length("5cm", True) == ureg.Quantity(0.05, "m")
-    l = nexgen.units_of_length("1in", True)
-    assert l.magnitude == 0.0254 and l.units == ureg.m
+    assert nexgen.units_of_length("1in", True) == ureg.Quantity(0.0254, "m")
 
 
 def test_units_of_time():
@@ -27,5 +26,4 @@ def test_units_of_time():
     # Check that a dimensionless value deafults to seconds
     assert nexgen.units_of_time(1) == ureg.Quantity(1, "s")
     # Check conversion to base units
-    t = nexgen.units_of_time("20ms")
-    assert t.magnitude == 0.02 and t.units == ureg.second
+    assert nexgen.units_of_time("20ms") == ureg.Quantity(0.02, "m")
