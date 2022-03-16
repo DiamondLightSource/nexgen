5ef8caf8597a5672632b724f0d366f030efb872c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-03-16 09:30:18 +0000

    Start on xml reading

--- src/nexgen/beamlines/I19-2_Tristan_nxs.py
+++ src/nexgen/beamlines/I19-2_Tristan_nxs.py
@@ -3,7 +3,8 @@ Create a NeXus file for time resolved collections on I19-2 Tristan 10M detector.
 """
 
 import sys
-import json
+
+# import json
 import logging
 
 from pathlib import Path
@@ -11,7 +12,11 @@ from pathlib import Path
 from collections import namedtuple
 from typing import Union
 
-# from .I19_2_params import source
+from .I19_2_params import (
+    # source,
+    goniometer_axes,
+    tristan10M_params,
+)
 
 # from .. import (
 #     get_iso_timestamp,
@@ -23,7 +28,7 @@ from typing import Union
 #     find_scan_axis,
 # )
 
-# from ..tools.ExtendedRequest import ExtendedRequestIO
+from ..tools.ExtendedRequest import ExtendedRequestIO
 
 # Define a logger object and a formatter
 logger = logging.getLogger("NeXusGenerator.I19-2")
@@ -43,50 +48,146 @@ tr_collect = namedtuple(
     [
         "meta_file",
         "xml_file",
-        "geometry_json",
-        "detector_json",
         "exposure_time",
         "wavelength",
-        "beam_pos_x",
-        "beam_pos_y",
+        "beam_center",  # This will have a command line call anyway because I can't call it from bash
+        # "beam_pos_x",
+        # "beam_pos_y",
         "start_time",
         "end_time",
         # "pump_status",
         # "pump_exp",
         # "pump_delay",
+        "geometry_json",  # Define these 2 ase None
+        "detector_json",
     ],
 )
 
 # Initialize dictionaries
-goniometer = {}
-detector = {}
+goniometer = {}  # goniometer_axes
+detector = {}  # tristan10M_params
 module = {}
-beam = {}
+beam = {"flux": None}
 attenuator = {}
 
 
-def read_geometry_from_json(axes_geometry: Union[Path, str]):
-    """_summary_
+def read_from_xml(xmlfile: Union[Path, str]):
+    ecr = ExtendedRequestIO(xmlfile)
 
-    Args:
-        axes_geometry (Union[Path, str]): _description_
+    # Attenuator
+    attenuator["transmission"] = ecr.getTransmission()
+
+    # Detector [2theta, det_z]
+    # detector["starts"] = [ecr.getTwoTheta(), ecr.getSampleDetectorDistance()]
+    detector["starts"] = [0.0, ecr.getSampleDetectorDistance()]
+
+    # Goniometer
+    osc_seq = ecr.getOscillationSequence()
+    # Find scan range
+    if osc_seq["range"] == 0.0:
+        scan_range = (osc_seq["start"], osc_seq["start"])
+    else:
+        start = osc_seq["start"]
+        num = osc_seq["number_of_images"]
+        stop = start + num * osc_seq["range"]
+        scan_range = (start, stop)
+    # Determine scan axis
+    if ecr.getAxisChoice() == "omega":
+        scan_axis = "omega"
+        omega_pos = (*scan_range, 0.0)
+        phi_pos = (*2 * (ecr.getOtherAxis()), 0.0)
+    else:
+        scan_axis = "phi"
+        phi_pos = (*scan_range, 0.0)
+        omega_pos = (*2 * (ecr.getOtherAxis(),), 0.0)
+    kappa_pos = (*2 * (ecr.getKappa(),), 0.0)
+
+    pos = {
+        "omega": omega_pos,
+        "phi": phi_pos,
+        "kappa": kappa_pos,
+        "sam_x": (0.0, 0.0, 0.0),
+        "sam_y": (0.0, 0.0, 0.0),
+        "sam_z": (0.0, 0.0, 0.0),
+    }
+
+    print(scan_axis, pos)
+
+
+def write_nxs(**tr_params):
     """
-    # Load information from JSON file
-    with open(axes_geometry, "r") as f:
-        geom = json.load(f)
-    print(geom)
-
-
-def read_detector_params_from_json(
-    detector_params: Union[Path, str],
-):
-    """_summary_
-
-    Args:
-        detector_params (Union[Path, str]): _description_
+    Gather all parameters from the beamline and call the NeXus writers.
     """
-    pass
+    # Get info from the beamline
+    TR = tr_collect(
+        meta_file=Path(tr_params["meta_file"]).expanduser().resolve(),
+        xml_file=Path(tr_params["xml_file"]).expanduser().resolve(),
+        exposure_time=tr_params["exposure_time"],
+        wavelength=tr_params["wavelength"],
+        beam_center=tr_params["beam_center"],
+        start_time=tr_params["start_time"].strftime("%Y-%m-%dT%H:%M:%S")
+        if tr_params["start_time"]
+        else None,  # This should be datetiem type
+        stop_time=tr_params["stop_time"].strftime("%Y-%m-%dT%H:%M:%S")
+        if tr_params["start_time"]
+        else None,  # idem.
+        geometry_json=None,  # tr_params["geometry_json"] if tr_params["geometry_json"] else None,
+        detector_json=None,  # tr_params["detector_json"] if tr_params["detector_json"] else None,
+    )
+
+    # Get goniometer and detector parameters
+    # FIXME I mean, it works but ... TODO
+    if TR.geometry_json:
+        # here call json reader
+        pass
+    else:
+        for k, v in goniometer_axes:
+            goniometer[k] = v
+
+    if TR.detector_json:
+        # idem aedem idem
+        pass
+    else:
+        for k, v in tristan10M_params:
+            detector[k] = v
+            # mah ...
 
 
-def write_nxs():
-    pass
+# # Example usage
+# if __name__ == "__main__":
+#     from datetime import datetime
+
+#     write_nxs(
+#         meta_file=sys.argv[1],
+#         xml_file=sys.argv[2],
+#         exposure_time=100,
+#         wavelength= 0.649,
+#         beam_center=[1590.7, 1643.7],
+#         start_time=datetime.now(),
+#         stop_time=datetime.now(),
+#         geometry_json=None,
+#         detector_json=None,
+#     )
+
+# TODO separated from this make some sort of json2params tool (see jupyter notebook)
+# def read_geometry_from_json(axes_geometry: Union[Path, str]):
+#     """_summary_
+
+#     Args:
+#         axes_geometry (Union[Path, str]): _description_
+#     """
+#     # Load information from JSON file
+#     with open(axes_geometry, "r") as f:
+#         geom = json.load(f)
+#     print(geom)
+
+
+# def read_detector_params_from_json(
+#     detector_params: Union[Path, str],
+# ):
+#     """_summary_
+
+#     Args:
+#         detector_params (Union[Path, str]): _description_
+#     """
+#     pass
--- src/nexgen/tools/ExtendedRequest.py
+++ src/nexgen/tools/ExtendedRequest.py
@@ -4,19 +4,22 @@ IO tool to gather beamline and collection information from xml file.
 
 import xml.etree.ElementTree as ET
 
+from pathlib import Path
+from typing import Dict, Tuple, Union
+
 
 class ExtendedRequestIO:
-    def __init__(self, xmlfile):
+    def __init__(self, xmlfile: Union[Path, str]):
         self.tree = ET.parse(xmlfile)
         self.root = self.tree.getroot()
 
-    def getCollectionInfo(self):
+    def getCollectionInfo(self) -> Tuple[str, str, str]:
         directory = self.root.find(".//directory").text
         prefix = self.root.find(".//prefix").text
         run_number = self.root.find(".//runNumber").text
         return directory, prefix, run_number
 
-    def getOscillationSequence(self):
+    def getOscillationSequence(self) -> Dict[str, float]:
         osc_seq_node = self.root.find(".//oscillation_sequence")
         osc_seq = {
             "start": float(osc_seq_node.find(".//start").text),
@@ -27,28 +30,28 @@ class ExtendedRequestIO:
         }
         return osc_seq
 
-    def getAxisChoice(self):
+    def getAxisChoice(self) -> str:
         ax = self.root.find(".//axisChoice").text
         return ax.lower()
 
-    def getOtherAxis(self):
+    def getOtherAxis(self) -> str:
         val = float(self.root.find(".//otherAxis").text)
         return val
 
-    def getKappa(self):
+    def getKappa(self) -> float:
         return float(self.root.find(".//kappa").text)
 
-    def getChi(self):
+    def getChi(self) -> float:
         return float(self.root.find(".//chi").text)
 
-    def getTwoTheta(self):
+    def getTwoTheta(self) -> float:
         return float(self.root.find(".//twoTheta").text)
 
-    def getSampleDetectorDistance(self):
+    def getSampleDetectorDistance(self) -> float:
         return float(self.root.find(".//sampleDetectorDistanceInMM").text)
 
-    def getTransmission(self):
+    def getTransmission(self) -> float:
         return float(self.root.find(".//transmissionInPerCent").text)
 
-    def getResolution(self):
+    def getResolution(self) -> float:
         return float(self.root.find(".//resolution").text)
