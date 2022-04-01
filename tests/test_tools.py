4c1408a9cf07b29688f34fa10385184894c0bdf3
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-04-01 11:38:01 +0100

    Comment out till 3.6 is dropped

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -13,6 +13,20 @@ def test_cif2nxs():
     assert nexgen.imgcif2mcstas([0, 0, 1]) == (0, 0, -1)
 
 
+def test_split_arrays():
+    assert nexgen.split_arrays("imgcif", ["phi"], [1, 0, 0]) == {"phi": (-1, 0, 0)}
+    two_axes = nexgen.split_arrays("mcstas", ["omega", "phi"], [1, 0, 0, 0, 1, 0])
+    assert two_axes["omega"] == (1, 0, 0) and two_axes["phi"] == (0, 1, 0)
+    assert (
+        len(
+            nexgen.split_arrays(
+                "mcstas", ["omega", "phi", "chi"], [1, 0, 0, 0, 1, 0, 0, 0, 1]
+            )
+        )
+        == 3
+    )
+
+
 def test_iso_timestamps():
     assert nexgen.get_iso_timestamp(None) is None
     # Check that no exceptions are raised when passing a time.time() object
