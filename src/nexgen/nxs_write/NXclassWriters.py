4350f5079c72ac06cb60ffb922d30c6f1ad6a52e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-07-14 12:38:38 +0100

    Write axis_increment_set as one single value instead of an array - try again

--- src/nexgen/nxs_write/NXclassWriters.py
+++ src/nexgen/nxs_write/NXclassWriters.py
@@ -257,8 +257,8 @@ def write_NXsample(
                 increment_set = np.repeat(goniometer["increments"][idx], len(osc_range))
                 nxsample_ax.create_dataset(
                     ax + "_increment_set",
-                    data=increment_set,
-                )  # goniometer["increments"][idx]
+                    data=goniometer["increments"][idx],
+                )  # increment_set
                 nxsample_ax.create_dataset(ax + "_end", data=osc_range + increment_set)
         elif ax in scan_axes:
             # For translations
