4a80af7f921e36b54926dc0622e6be9de6eeb497
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-07-27 10:07:49 +0100

    Try to fix error in tests

--- src/nexgen/__init__.py
+++ src/nexgen/__init__.py
@@ -1,13 +1,12 @@
 """
 General tools useful to create NeXus format files.
 """
+from __future__ import annotations
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
 __version__ = "0.6.9"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
-
-from __future__ import annotations
 
 import logging
 import re
