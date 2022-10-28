f7b95823eb9052b7342f008dc9de0a9a525f5c46
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-10-28 11:18:14 +0100

    Set value to 0 instead of False in broken test

--- tests/test_tools_for_ED.py
+++ tests/test_tools_for_ED.py
@@ -86,7 +86,7 @@ def test_get_mask_and_flatfield_from_singla_master_file(dummy_singla_master_file
     D = extract_from_SINGLA_master(dummy_singla_master_file.name)
     assert "pixel_mask" in D.keys() and "flatfield" in D.keys()
     assert D["pixel_mask"] is None
-    assert D["pixel_mask_applied"] is False
+    assert D["pixel_mask_applied"] == 0
     assert np.all(D["flatfield"] == test_flatfield) is True
     assert D["flatfield_applied"] is None
 
