676a5da445ce3f6946a7cc3430b874edfa295ed6
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-11-09 19:45:55 +0000

    Bump version: 0.6.11 → 0.6.12

--- src/nexgen/__init__.py
+++ src/nexgen/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.6.11"
+__version__ = "0.6.12"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 import logging
