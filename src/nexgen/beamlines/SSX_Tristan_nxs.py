5a951326d7779dce8b7adfafd0a2cf6148a882a1
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2022-11-23 17:17:09 +0000

    Tidy up

--- src/nexgen/beamlines/SSX_Tristan_nxs.py
+++ src/nexgen/beamlines/SSX_Tristan_nxs.py
@@ -229,11 +229,11 @@ def write_nxs(**ssx_params):
 
                 chip = read_chip_map(SSX_TR.chipmap, 8, 8)
                 mapping = {"chipmap": str(chip)}
-                # nxsfile.create_dataset("/entry/data/chipmap", data=str(chip))
+                logger.info(
+                    f"Chipmap read from {SSX_TR.chipmap}, saving in '/entry/source/notes/chipmap'."
+                )
                 write_NXnote(nxsfile, "/entry/source/notes/", mapping)
                 # To read this: eval(dset[()])
-                # Instead of saving chipmap location read file and save dictionary.
-                # TODO add part to copy functions to get the positions.
 
             # Register pump status (hard coded as True)
             pump_info = {"pump_status": True}
