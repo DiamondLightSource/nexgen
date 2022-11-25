0b1e2f991de6e3016e7895cf92f0a0bcac76c4f3
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2022-11-25 14:46:37 +0000

    Bump version: 0.6.13 → 0.6.14

--- src/nexgen/__init__.py
+++ src/nexgen/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.6.13"
+__version__ = "0.6.14"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 import logging
