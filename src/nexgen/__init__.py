a929c38c66b491d949db0755a2b24b5dd9d55d3e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-01-12 16:57:50 +0000

    Fix author email

--- src/nexgen/__init__.py
+++ src/nexgen/__init__.py
@@ -4,7 +4,7 @@ General tools useful to create NeXus format files.
 from __future__ import annotations
 
 __author__ = "Diamond Light Source - Scientific Software"
-__email__ = "scientificsoftware@diamond.ac.uk"
+__email__ = "data_analysis@diamond.ac.uk"
 __version__ = "0.6.16"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
