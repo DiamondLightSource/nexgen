8521b01f226f6e1c6dad9b8beafde15bc183ce4f
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-07-10 16:36:32 +0100

    Add some tests for gda tools and tidy up

---
+++ tests/beamlines/conftest.py
@@ -0,0 +1,97 @@
+import json
+import tempfile
+
+import pytest
+
+
+@pytest.fixture
+def dummy_xml_file():
+    test_xml = """<?xml version="1.0" encoding="UTF-8"?>
+        <ExtendedCollectRequests>
+            <fileinfo>
+                <directory>/path/to/data</directory>
+                <prefix>Filename</prefix>
+            </fileinfo>
+            <runNumber>1</runNumber>
+            <oscillation_sequence>
+                <start>-180.0</start>
+                <range>2.0</range>
+                <number_of_images>10</number_of_images>
+                <exposure_time>0.2</exposure_time>
+                <number_of_passes>1</number_of_passes>
+            </oscillation_sequence>
+            <sampleDetectorDistanceInMM>110.0</sampleDetectorDistanceInMM>
+            <transmissionInPerCent>10.0</transmissionInPerCent>
+            <kappa>0.0</kappa>
+            <axisChoice>Omega</axisChoice>
+            <twoTheta>10.0</twoTheta>
+            <otherAxis>-90.0</otherAxis>
+            <resolution>1</resolution>
+        </ExtendedCollectRequests>
+    """
+    test_xml_file = tempfile.NamedTemporaryFile(suffix=".xml", delete=True)
+    with open(test_xml_file.name, "w") as f:
+        f.write(test_xml)
+    yield test_xml_file
+
+
+@pytest.fixture
+def dummy_geometry_json():
+    test_geometry = {
+        "sample_omega": {
+            "depends_on": ".",
+            "ds_name": "omega",
+            "vector": [-1, 0, 0],
+            "location": "sample",
+            "units": "deg",
+            "type": "rotation",
+        },
+        "geometry": "mcstas",
+        "sample_x": {
+            "depends_on": "omega",
+            "ds_name": "sam_x",
+            "vector": [1, 0, 0],
+            "location": "sample",
+            "units": "mm",
+            "type": "translation",
+        },
+        "phi": {
+            "depends_on": "sam_x",
+            "ds_name": "phi",
+            "vector": [0, 0, -1],
+            "location": "sample",
+            "units": "deg",
+            "type": "rotation",
+        },
+        "detector_z": {
+            "depends_on": ".",
+            "ds_name": "det_z",
+            "vector": [0, 0, 1],
+            "location": "detector",
+            "units": "mm",
+            "type": "translation",
+        },
+    }
+    test_json_file = tempfile.NamedTemporaryFile(suffix=".json", delete=True)
+    with open(test_json_file.name, "w") as f:
+        json.dump(test_geometry, f, indent=4)
+    yield test_json_file
+
+
+@pytest.fixture
+def dummy_detector_json():
+    test_detector = {
+        "eiger": {
+            "pixel_size_units": "m",
+            "description": "Eiger 2X",
+            "sensor_material": "CdTe",
+            "pixel_size": [7.5e-05, 7.5e-05],
+            "size": [2162, 2068],
+            "fast_dir": [0, 1, 0],
+            "slow_dir": [-1, 0, 0],
+        }
+    }
+    test_json_file = tempfile.NamedTemporaryFile(suffix=".json", delete=True)
+    with open(test_json_file.name, "w") as f:
+        json.dump(test_detector, f, indent=4)
+    yield test_json_file
--- tests/beamlines/test_GDA_tools.py
+++ tests/beamlines/test_GDA_tools.py
@@ -1,35 +1,63 @@
+from nexgen.beamlines.GDAtools.ExtendedRequest import (
+    ExtendedRequestIO,
+    read_det_position_from_xml,
+    read_scan_from_xml,
+)
 from nexgen.beamlines.GDAtools.GDAjson2params import JSONParamsIO
 
-GEOMETRY_JSON = "tests/beamlines/test_geometry.json"
-DETECTOR_JSON = "tests/beamlines/test_detector.json"
+
+def test_get_coordinate_frame_from_json(dummy_geometry_json):
+    assert JSONParamsIO(dummy_geometry_json.name).get_coordinate_frame() == "mcstas"
 
 
-def test_get_coordinate_frame_from_json():
-    assert JSONParamsIO(GEOMETRY_JSON).get_coordinate_frame() == "mcstas"
-
-
-def test_get_gonio_axes_from_json():
-    gonio_axes = JSONParamsIO(GEOMETRY_JSON).get_goniometer_axes_from_file()
-    assert type(gonio_axes) is list and len(gonio_axes) == 2
+def test_get_gonio_axes_from_json(dummy_geometry_json):
+    gonio_axes = JSONParamsIO(dummy_geometry_json.name).get_goniometer_axes_from_file()
+    assert type(gonio_axes) is list and len(gonio_axes) == 3
     assert gonio_axes[0].name == "omega" and gonio_axes[1].name == "sam_x"
+    assert gonio_axes[2].name == "phi" and gonio_axes[2].depends == "sam_x"
 
 
-def test_get_detector_axes_from_json():
-    det_axes = JSONParamsIO(GEOMETRY_JSON).get_detector_axes_from_file()
+def test_get_detector_axes_from_json(dummy_geometry_json):
+    det_axes = JSONParamsIO(dummy_geometry_json.name).get_detector_axes_from_file()
     assert type(det_axes) is list and len(det_axes) == 1
     assert det_axes[0].name == "det_z"
 
 
-def test_get_fast_and_slow_dir_from_json():
+def test_get_fast_and_slow_dir_from_json(dummy_detector_json):
     fast, slow = JSONParamsIO(
-        DETECTOR_JSON
+        dummy_detector_json.name
     ).get_fast_and_slow_direction_vectors_from_file("eiger")
     assert (fast.x, fast.y, fast.z) == (0, 1, 0)
     assert (slow.x, slow.y, slow.z) == (-1, 0, 0)
 
 
-def test_get_detector_params_from_json():
-    params = JSONParamsIO(DETECTOR_JSON).get_detector_params_from_file()
+def test_get_detector_params_from_json(dummy_detector_json):
+    params = JSONParamsIO(dummy_detector_json.name).get_detector_params_from_file()
     assert params.description == "Eiger 2X"
     assert params.image_size == [2162, 2068]
     assert params.pixel_size == ["7.5e-05m", "7.5e-05m"]
+
+
+def test_ExtendedRequestIO(dummy_xml_file):
+    test_ecr = ExtendedRequestIO(dummy_xml_file.name)
+    assert test_ecr.getAxisChoice() == "omega"
+    assert test_ecr.getCollectionInfo() == ("/path/to/data", "Filename", "1")
+    assert test_ecr.getTransmission() == 10.0
+    assert test_ecr.getResolution() == 1.0
+
+
+def test_read_scan_from_xml(dummy_xml_file):
+    test_ecr = ExtendedRequestIO(dummy_xml_file.name)
+    scan_axis, pos, num = read_scan_from_xml(test_ecr)
+    assert scan_axis == test_ecr.getAxisChoice()
+    assert num == 10
+    assert type(pos) is dict and len(pos) == 6  # gonio axes on I19-2
+    assert pos["omega"] == (-180.0, -160.0, 2)
+    assert pos["phi"] == (*2 * (test_ecr.getOtherAxis(),), 0.0)
+
+
+def test_read_det_position_from_xml(dummy_xml_file):
+    test_ecr = ExtendedRequestIO(dummy_xml_file.name)
+    det_pos = read_det_position_from_xml(test_ecr, "Eiger")
+    assert det_pos[0] == test_ecr.getTwoTheta()
+    assert det_pos[1] == test_ecr.getSampleDetectorDistance()
