42bf1177d7ae0effa7c48951a6e374aa86b9de88
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2023-08-30 15:49:04 +0100

    Add __main__ file

---
+++ src/nexgen/__main__.py
@@ -0,0 +1,10 @@
+#from . import __version__
+
+from .command_line import version_parser
+
+def main(args=None):
+    args = version_parser.parse_args(args)
+
+# Test with python -m nexgen
+if __name__ == "__main__":
+    main()
