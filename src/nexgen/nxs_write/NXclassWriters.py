bf8ce9fe28745693c9cccf7a21657afbb73c8995
Noemi Frisina <54588199+noemifrisina@users.noreply.github.com>
2023-10-06 17:53:56 +0100

    Add detector_z in NXdetector (#139)

--- src/nexgen/nxs_write/NXclassWriters.py
+++ src/nexgen/nxs_write/NXclassWriters.py
@@ -665,6 +665,12 @@ def write_NXdetector(
                 data=set_dependency(ax, path=nxgrp_ax.name),
             )
 
+    # Write a soft link for detector_z
+    if "detector_z" in list(nxtransformations.keys()):
+        nxdetector["detector_z"] = nxsfile[
+            "/entry/instrument/detector/transformations/detector_z"
+        ]
+
     # Detector distance
     nxdetector.create_dataset("distance", data=dist.to("m").magnitude)
     create_attributes(
