7d04bb2d45920f4aaa69d79783344b3a05eff005
Marko Petrovic <mpetrovic.ks@gmail.com>
2024-10-06 20:00:38 +0100

    Fix ruff error

--- src/nexgen/command_line/ED_mrc_to_nexus.py
+++ src/nexgen/command_line/ED_mrc_to_nexus.py
@@ -9,11 +9,15 @@ from pathlib import Path
 
 import numpy as np
 
-from nexgen.beamlines.ED_params import (ED_coord_system, EDCeta,
-                                        EDSource, UnknownSource)
-
-from nexgen.nxs_utils import (Attenuator, Beam, CetaDetector, TVIPSDetector,
-                              Detector, Goniometer)
+from nexgen.beamlines.ED_params import ED_coord_system, EDCeta, EDSource, UnknownSource
+from nexgen.nxs_utils import (
+    Attenuator,
+    Beam,
+    CetaDetector,
+    Detector,
+    Goniometer,
+    TVIPSDetector,
+)
 from nexgen.nxs_utils.scan_utils import calculate_scan_points
 from nexgen.nxs_write.ed_nxmx_writer import EDNXmxFileWriter
 from nexgen.tools.mrc_tools import collect_data, get_metadata
@@ -30,7 +34,7 @@ def main():
     parser.add_argument(
         "--detector",
         type=str,
-        default='cetad',
+        default="cetad",
         help="Detector type: use --detector cetad or --detector tvips",
     )
 
@@ -48,10 +52,10 @@ def main():
         help="Short name of the facility (e.g. DLS)."
     )
 
-    msg = 'Facility ID (e.g. DIAMOND). '
-    msg += 'Naming tries to follow the recommended convention for NXmx: '
-    msg += 'https://mmcif.wwpdb.org/dictionaries/'
-    msg += 'mmcif_pdbx_v50.dic/Items/_diffrn_source.type.html'
+    msg = "Facility ID (e.g. DIAMOND). "
+    msg += "Naming tries to follow the recommended convention for NXmx: "
+    msg += "https://mmcif.wwpdb.org/dictionaries/"
+    msg += "mmcif_pdbx_v50.dic/Items/_diffrn_source.type.html"
 
     parser.add_argument(
         "--facility-id",
@@ -106,22 +110,22 @@ def main():
         default=None,
         help="Data type for the HDF5 output (int32, int64, float64, ...)"
     )
-    des = 'List of input files. Can be a single MRC file containing all the '
-    des += 'images, or a list of files containing single images, '
-    des += 'usually obtained by global expansion (e.g. *mrc)'
+    des = "List of input files. Can be a single MRC file containing all the "
+    des += "images, or a list of files containing single images, "
+    des += "usually obtained by global expansion (e.g. *mrc)"
     parser.add_argument('input_files', nargs='+', help=des)
 
     args = parser.parse_args()
 
-    logger.info('Starting MRC to Nexus conversion')
+    logger.info("Starting MRC to Nexus conversion")
 
     for file in args.input_files:
-        if not file.endswith('.mrc'):
-            raise ValueError('Not an MRC file: %s' % file)
+        if not file.endswith(".mrc"):
+            raise ValueError("Not an MRC file: %s" % file)
 
     mrc_files = args.input_files
     full_mrc_path = os.path.abspath(mrc_files[0])
-    logger.info('Collecting MRC data into a h5 file')
+    logger.info("Collecting MRC data into a h5 file")
     tot_imgs, out_file, angles = collect_data(mrc_files)
 
     mdict = get_metadata(mrc_files[0])
@@ -129,62 +133,59 @@ def main():
 
     if args.det_distance is not None:
         det_distance = args.det_distance
-    elif 'cameraLength' in mdict:
-        det_distance = mdict['cameraLength']
-        msg = 'Reading detector distance from '
-        msg += 'the MRC files: %.1f' % det_distance
+    elif "cameraLength" in mdict:
+        det_distance = mdict["cameraLength"]
+        msg = "Reading detector distance from "
+        msg += "the MRC files: %.1f" % det_distance
         logger.info(msg)
     else:
-        msg = 'No detector distance in the MRC metadata. '
-        msg += 'You can set it with the --det-distance option.'
+        msg = "No detector distance in the MRC metadata. "
+        msg += "You can set it with the --det-distance option."
         raise ValueError(msg)
 
     if args.wavelength is not None:
         beam = Beam(args.wavelength)
-    elif 'wavelength' in mdict:
-        beam = Beam(mdict['wavelength'])
-        msg = ('Reading wavelength from the MRC files: %f' %
+    elif "wavelength" in mdict:
+        beam = Beam(mdict["wavelength"])
+        msg = ("Reading wavelength from the MRC files: %f" %
                mdict['wavelength'])
         logger.info(msg)
     else:
-        msg = 'No wavelength in the MRC metadata. '
-        msg += 'You can set it with the --wavelength option.'
+        msg = "No wavelength in the MRC metadata. "
+        msg += "You can set it with the --wavelength option."
         raise ValueError(msg)
 
     if args.angle_start is not None:
         start_angle = args.angle_start
     elif angles[0] is not None:
         start_angle = angles[0]
-        msg = ('Reading starting angle from the MRC files: %.2f' %
-               start_angle)
+        msg = ("Reading starting angle from the MRC files: %.2f" % start_angle)
         logger.info(msg)
     else:
-        msg = 'No starting angle in the MRC metadata. '
-        msg += 'You can set it with the --angle-start option.'
+        msg = "No starting angle in the MRC metadata. "
+        msg += "You can set it with the --angle-start option."
         raise ValueError(msg)
 
     if args.angle_inc is not None:
         increment = args.angle_inc
-    elif 'tiltPerImage' in mdict:
-        increment = mdict['tiltPerImage']
-        msg = ('Reading angle increment from the MRC files: %f' %
-               increment)
+    elif "tiltPerImage" in mdict:
+        increment = mdict["tiltPerImage"]
+        msg = ("Reading angle increment from the MRC files: %f" % increment)
         logger.info(msg)
     else:
-        msg = 'No angle increment in the MRC metadata. '
-        msg += 'You can set it with the --angle-inc option.'
+        msg = "No angle increment in the MRC metadata. "
+        msg += "You can set it with the --angle-inc option."
         raise ValueError(msg)
 
     if args.exp_time is not None:
         exposure_time = args.exp_time
-    elif 'integrationTime' in mdict:
-        exposure_time = mdict['integrationTime']
-        msg = ('Reading exposure time from the MRC files: %f' %
-               exposure_time)
+    elif "integrationTime" in mdict:
+        exposure_time = mdict["integrationTime"]
+        msg = ("Reading exposure time from the MRC files: %f" % exposure_time)
         logger.info(msg)
     else:
-        msg = 'No exposure time in the MRC metadata. '
-        msg += 'You can set it with --exp-time option.'
+        msg = "No exposure time in the MRC metadata. "
+        msg += "You can set it with --exp-time option."
         raise ValueError(msg)
 
     if args.beam_center is not None:
@@ -192,17 +193,17 @@ def main():
             x, y = args.beam_center
             beam_center = (x, y)
         else:
-            msg = 'Beam center requires two arguments'
+            msg = "Beam center requires two arguments"
             raise ValueError(msg)
-    elif ('beamCentreXpx' in mdict) and ('beamCentreYpx' in mdict):
+    elif ("beamCentreXpx" in mdict) and ("beamCentreYpx" in mdict):
         beam_center = (mdict["beamCentreXpx"],
                        mdict["beamCentreYpx"])
-        msg = ('Reading beam center from the MRC files: (%.1f, %.1f)' %
+        msg = ("Reading beam center from the MRC files: (%.1f, %.1f)" %
                beam_center)
         logger.info(msg)
     else:
-        msg = 'No beam center in the MRC metadata. '
-        msg += 'You can set it with --beam-center option.'
+        msg = "No beam center in the MRC metadata. "
+        msg += "You can set it with --beam-center option."
         raise ValueError(msg)
 
     gonio_axes = EDCeta.gonio
@@ -215,12 +216,12 @@ def main():
                                  tot_num_imgs=tot_imgs)
     goniometer = Goniometer(gonio_axes, scan)
 
-    nx = mdict['nx']
-    ny = mdict['ny']
+    nx = mdict["nx"]
+    ny = mdict["ny"]
 
-    if args.detector == 'tvips':
+    if args.detector == "tvips":
 
-        logger.info('Detector set to tvips')
+        logger.info("Detector set to tvips")
         det_params = TVIPSDetector("TVIPS Detector", [nx, ny])
         if (nx == 4096) and (ny == 4096):
             binning = 1
@@ -232,7 +233,7 @@ def main():
             binning = 8
         else:
             msg = f"Non standard detector size ({nx}, {ny})."
-            msg += 'Expecting (4096/b, 4096/b) with binning b = 1, 2, 4, 8'
+            msg += "Expecting (4096/b, 4096/b) with binning b = 1, 2, 4, 8"
             raise ValueError(msg)
 
         pixel_size = 0.015500 * binning
@@ -243,11 +244,11 @@ def main():
             source.short_name = args.facility_short
         if args.facility_id:
             source.facility_id = args.facility_id
-        source.beamline = 'TVIPS'
+        source.beamline = "TVIPS"
 
-    elif args.detector == 'cetad':
+    elif args.detector == "cetad":
 
-        logger.info('Detector set to cetad')
+        logger.info("Detector set to cetad")
         det_params = CetaDetector("Thermo Fisher Ceta-D", [nx, ny])
 
         if (nx == 2048) and (ny == 2048):
@@ -258,7 +259,7 @@ def main():
         pixel_size = 0.014 * binning
         det_params.overload = 8000 * binning**2
         source = EDSource
-        source.beamline = 'CetaD-eBIC'
+        source.beamline = "CetaD-eBIC"
 
     else:
         msg = f"Unkown detector type {args.detector}."
@@ -271,10 +272,10 @@ def main():
     extra_params = {}
     mask = np.zeros((nx, ny), dtype=np.int32)
     flatfield = np.ones((nx, ny), dtype=np.float32)
-    extra_params['pixel_mask'] = mask
-    extra_params['flatfield'] = flatfield
-    extra_params['pixel_mask_applied'] = 1
-    extra_params['flatfield_applied'] = 1
+    extra_params["pixel_mask"] = mask
+    extra_params["flatfield"] = flatfield
+    extra_params["pixel_mask_applied"] = 1
+    extra_params["flatfield_applied"] = 1
     det_params.constants.update(extra_params)
 
     det_axes = EDCeta.det_axes
@@ -287,12 +288,12 @@ def main():
                          EDCeta.slow_axis])
 
     script_dir = os.path.dirname(full_mrc_path)
-    file_name = out_file.replace('h5', 'nxs')
+    file_name = out_file.replace("h5", "nxs")
     full_path = os.path.join(script_dir, file_name)
     data_path = os.path.join(script_dir, out_file)
     data_path = Path(data_path)
 
-    msg = 'Writing the Nexus file %s' % full_path
+    msg = "Writing the Nexus file %s" % full_path
     logger.info(msg)
     writer = EDNXmxFileWriter(
          full_path,
@@ -305,7 +306,7 @@ def main():
          ED_coord_system)
 
     datafiles = [data_path]
-    writer.write(datafiles, '/entry/data/data')
+    writer.write(datafiles, "/entry/data/data")
     writer.write_vds(vds_dtype=np.int32,
                      datafiles=datafiles)
-    logger.info('MRC images converted to Nexus.')
+    logger.info("MRC images converted to Nexus.")
