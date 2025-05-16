213b7d12bff6b0620431f1afed7f976d10e3bfc1
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-05-16 16:39:27 +0100

    Fix test

--- tests/beamlines/test_i19nxs.py
+++ tests/beamlines/test_i19nxs.py
@@ -5,7 +5,12 @@ from unittest.mock import patch
 
 import pytest
 
-from nexgen.beamlines.I19_2_nxs import ExperimentTypeError, eiger_writer, nexus_writer
+from nexgen.beamlines.I19_2_nxs import (
+    CollectionParams,
+    ExperimentTypeError,
+    eiger_writer,
+    nexus_writer,
+)
 
 
 def test_nexus_writer_raises_error_for_serial():
@@ -19,7 +24,13 @@ def test_nexus_writer_fails_if_missing_n_imgs_and_not_using_meta(fake_log):
         nexus_writer("/path/to/meta", "eiger", 0.1, use_meta=False)
 
 
-@patch("nexgen.beamlines.I19_2_nxs.CollectionParams")
-def test_eiger_nxs_writer_fails_if_missing_axes_and_no_meta(fake_params):
+def test_eiger_nxs_writer_fails_if_missing_axes_and_no_meta():
+    fake_params = CollectionParams(
+        exposure_time=0.01,
+        beam_center=(100, 200),
+        wavelength=0.4,
+        metafile="somefile",
+        detector_name="eiger",
+    )
     with pytest.raises(ValueError):
         eiger_writer(Path("/path/to/meta"), fake_params, use_meta=False)
