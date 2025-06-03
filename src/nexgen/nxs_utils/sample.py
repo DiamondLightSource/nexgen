ae8a04f3bf0d20facecc3be73bc03082af6409b6
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-06-03 13:50:34 +0100

    Tidy up a couple of tests

--- src/nexgen/nxs_utils/sample.py
+++ src/nexgen/nxs_utils/sample.py
@@ -11,7 +11,7 @@ from pydantic.dataclasses import dataclass
 class Sample:
     name: str | None = None
     depends_on: str | None = None
-    temperature: str | None = None
+    temperature: float | None = None
 
     def to_dict(self) -> dict:
         return self.__dict__
