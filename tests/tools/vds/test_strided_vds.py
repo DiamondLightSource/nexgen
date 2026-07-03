4e1a8585d88ca7e6017a5e2329ea1dea350a8edd
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-07-03 11:14:53 +0100

    Maybe save all files

---
+++ tests/tools/vds/test_strided_vds.py
@@ -0,0 +1,47 @@
+from unittest.mock import patch
+
+import numpy as np
+
+from nexgen.tools.vds_tools.strided_mapping import (
+    SingleDataset,
+    create_dataset_list,
+    create_vds_layout,
+    write_strided_vds,
+)
+
+
+def test_create_dataset_list(nexus_file_with_multiple_datasets):
+    dsets = create_dataset_list(
+        nexus_file_with_multiple_datasets["/entry/data"], start_index=0
+    )
+
+    assert len(dsets) == 2
+    assert dsets[0].name == "data_0001"
+    assert dsets[1].name == "data_0002"
+
+    for dset in dsets:
+        assert dset.start_index == 0
+        assert dset.stride == 2
+
+
+def test_create_vds_layout():
+    dset_list = [
+        SingleDataset(name="data_01", src_shape=(6, 2, 2), start_index=1, stride=2)
+    ]
+    expected_shape = (3, 2, 2)
+
+    layout = create_vds_layout(dset_list, expected_shape, np.uint16)
+
+    assert layout.shape == expected_shape
+    assert list(layout._src_filenames)[0] == b"."
+
+
+@patch("nexgen.tools.vds_tools.strided_mapping.create_vds_layout")
+def test_write_strided_vds(mock_layout, nexus_file_with_multiple_datasets):
+    with patch(
+        "nexgen.tools.vds_tools.strided_mapping.h5py.Group.create_virtual_dataset"
+    ) as mock_create:
+        write_strided_vds(nexus_file_with_multiple_datasets, (10, 2, 3), 0)
+
+        mock_layout.assert_called_once()
+        mock_create.assert_called_once()
