bf0b397eeb68e4da8a2bbdecccbc5f8a8daf5a26
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-09-04 18:08:07 +0100

    Start thinking of tristan refactor

---
+++ src/nexgen/beamlines/i19_2/tristan.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from typing import Any
+
+from nexgen.beamlines.i19_2.parameters import CollectionParams
+
+
+def tristan_writer(
+    parameters: CollectionParams,
+    master_file: Path | str | None = None,
+    notes: dict[str, Any] | None = None,
+):
+    pass
+
+
+def start_writer():
+    pass
